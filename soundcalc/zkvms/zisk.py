0fc4f1b66d578c23a22c14a41adedb1a3151ec1d
George Kadianakis <desnacked@riseup.net>
2025-11-28 13:53:39 +0200

    Prep work: Add a dummy circuit to ZisK

--- soundcalc/zkvms/zisk.py
+++ soundcalc/zkvms/zisk.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 
-from soundcalc.zkvms.fri_based_vm import FRIBasedVM, FRIBasedVMConfig
+from soundcalc.zkvms.fri_based_vm import FRIBasedCircuit, FRIBasedVM, FRIBasedVMConfig
 
 from ..common.fields import *
 
 
 class ZiskPreset:
     @staticmethod
-    def default() -> "ZiskPreset":
+    def default() -> FRIBasedVM:
         """
-        Populate a zkEVMConfig instance with zkVM parameters.
+        Create a ZisK VM with multiple circuits.
 
         For ZisK, we populate the trace parameters from its constraint description:
             https://github.com/0xPolygonHermez/zisk/blob/main/pil/zisk.pil
@@ -17,11 +17,20 @@ class ZiskPreset:
         The rest of the parameters are adapted from the "eSTARK: Extending STARKs with Arguments" paper:
            https://eprint.iacr.org/2023/474
         """
+        return FRIBasedVM(
+            name="ZisK",
+            circuits=[
+                ZiskPreset._main_circuit(),
+                ZiskPreset._secondary_circuit(),
+            ]
+        )
 
+    @staticmethod
+    def _main_circuit() -> FRIBasedCircuit:
+        """
+        Main ZisK circuit - the largest trace.
+        """
         field = GOLDILOCKS_3
-
-        # We generate a STARK proof of different traces with different parameters.
-        # Therefore, in what follows, I put the parameters for our worst-case trace in terms of area.
 
         # The blowup factor is dinamically chosen by this tool:
         #       https://github.com/0xPolygonHermez/pil2-proofman-js/blob/main/src/pil2-stark/pil_info/imPolsCalculation/imPolynomials.js#L96
@@ -32,7 +41,7 @@ class ZiskPreset:
 
         trace_length = 1 << 22
         num_columns = 66
-        batch_size = num_columns + 2 # +2 for the composition polynomials
+        batch_size = num_columns + 2  # +2 for the composition polynomials
 
         num_queries = 128 // int(math.log2(blowup_factor))
 
@@ -46,10 +55,10 @@ class ZiskPreset:
 
         max_combo = 3
 
-        hash_size_bits = 256 # TODO: check if that is actually true
+        hash_size_bits = 256  # TODO: check if that is actually true
 
         cfg = FRIBasedVMConfig(
-            name="ZisK",
+            name="main",
             hash_size_bits=hash_size_bits,
             rho=rho,
             trace_length=trace_length,
@@ -64,4 +73,51 @@ class ZiskPreset:
             max_combo=max_combo,
             grinding_query_phase=0,
         )
-        return FRIBasedVM(cfg)
+        return FRIBasedCircuit(cfg)
+
+    @staticmethod
+    def _secondary_circuit() -> FRIBasedCircuit:
+        """
+        Secondary ZisK circuit - smaller trace (placeholder values).
+        """
+        field = GOLDILOCKS_3
+
+        blowup_factor = 2
+        rho = 1 / blowup_factor
+
+        trace_length = 1 << 21  # Smaller trace
+        num_columns = 50  # Fewer columns
+        batch_size = num_columns + 2
+
+        num_queries = 128 // int(math.log2(blowup_factor))
+
+        AIR_max_degree = blowup_factor + 1
+
+        # D = trace_length / rho = 2^22
+        # Product of factors should equal D / early_stop = 2^22 / 2^5 = 2^17
+        # [16, 16, 16, 8] = 2^4 * 2^4 * 2^4 * 2^3 = 2^15... need adjustment
+        # [16, 16, 8, 8, 8] = 2^4 * 2^4 * 2^3 * 2^3 * 2^3 = 2^17 ✓
+        FRI_folding_factors = [2**4, 2**4, 2**3, 2**3, 2**3]
+        FRI_early_stop_degree = 2**5
+
+        max_combo = 3
+
+        hash_size_bits = 256
+
+        cfg = FRIBasedVMConfig(
+            name="secondary",
+            hash_size_bits=hash_size_bits,
+            rho=rho,
+            trace_length=trace_length,
+            field=field,
+            num_columns=num_columns,
+            batch_size=batch_size,
+            power_batching=True,
+            num_queries=num_queries,
+            AIR_max_degree=AIR_max_degree,
+            FRI_folding_factors=FRI_folding_factors,
+            FRI_early_stop_degree=FRI_early_stop_degree,
+            max_combo=max_combo,
+            grinding_query_phase=0,
+        )
+        return FRIBasedCircuit(cfg)
